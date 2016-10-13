1eac980af49d583219475820480fdf3464753daa
alexanderthedev <alexprift@yahoo.gr>
2016-10-14 00:08:42 +0200

    till migration

---
+++ Vidly/ViewModel/RandomMovieVieModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration.Configuration;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.ViewModel
+{
+    public class RandomMovieVieModel
+    {
+        public Movie Movie { get; set; }
+        public List<Customer> Customers { get; set; }
+    }
+
+}
